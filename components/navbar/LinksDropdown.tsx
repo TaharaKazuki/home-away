66ce3331620568bc2df78c0743afb78611944b73
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-12 16:24:18 +0900

    update

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,5 +1,36 @@
+import Link from 'next/link';
+import { LuAlignLeft } from 'react-icons/lu';
+
+import UserIcon from '@/components/navbar/UserIcon';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { links } from '@/utils/links';
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex max-w-[100px] gap-4">
+          <LuAlignLeft className="size-6" />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="w-full capitalize">
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,7 @@
+import { LuUser2 } from 'react-icons/lu';
+
 function UserIcon() {
-  return <div>UserIcon</div>;
+  return <LuUser2 className="size-6 rounded-full bg-primary text-white" />;
 }
 
 export default UserIcon;
