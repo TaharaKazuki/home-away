abf368954f6bf228a0101bd387b2af55c5c32f65
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-12 17:40:25 +0900

    update

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -2,17 +2,21 @@
 import { type ReactNode } from 'react';
 
 import { ThemeProvider } from './theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 
 function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,12 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 import Link from 'next/link';
 import { LuAlignLeft } from 'react-icons/lu';
 
+import SignOutLink from './SignOutLink';
 import UserIcon from '@/components/navbar/UserIcon';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { links } from '@/utils/links';
@@ -21,16 +24,47 @@ function LinksDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
-        {links.map((link) => (
-          <DropdownMenuItem key={link.href}>
-            <Link href={link.href} className="w-full capitalize">
-              {link.label}
-            </Link>
-          </DropdownMenuItem>
-        ))}
+        <SignedOutStatus />
+        <SignInStatus />
       </DropdownMenuContent>
     </DropdownMenu>
   );
 }
+
+const SignInStatus = () => {
+  return (
+    <SignedIn>
+      {links.map((link) => (
+        <DropdownMenuItem key={link.href}>
+          <Link href={link.href} className="w-full capitalize">
+            {link.label}
+          </Link>
+        </DropdownMenuItem>
+      ))}
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <SignOutLink />
+      </DropdownMenuItem>
+    </SignedIn>
+  );
+};
+
+const SignedOutStatus = () => {
+  return (
+    <SignedOut>
+      <DropdownMenuItem>
+        <SignInButton mode="modal">
+          <button className="w-full text-left">Login</button>
+        </SignInButton>
+      </DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <SignUpButton mode="modal">
+          <button className="w-full text-left">Register</button>
+        </SignUpButton>
+      </DropdownMenuItem>
+    </SignedOut>
+  );
+};
 
 export default LinksDropdown;
