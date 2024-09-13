aaa6323146b5482ec3e7a33d70718fb7f813fc6b
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-13 15:32:17 +0900

    update

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,9 @@
 'use server';
 
+import { clerkClient, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+import db from './db';
 import { profileSchema } from './schemas';
 
 export const createProfileAction = async (
@@ -7,12 +11,29 @@ export const createProfileAction = async (
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
     const rawData = Object.fromEntries(formData);
     const validationFields = profileSchema.parse(rawData);
-    console.info('validationFields', validationFields);
-    return { message: 'profile created' };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validationFields,
+      },
+    });
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.error('エラーだよ', error);
-    return { message: 'there was an error' };
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
   }
+  redirect('/');
 };
