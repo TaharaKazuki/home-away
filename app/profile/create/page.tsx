25b77962167957b01b33ec316b52b255e0ab144f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-13 12:21:25 +0900

    update

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,25 +1,27 @@
 import { SubmitButton } from '@/components/form/Button';
+import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server';
   const firstName = formData.get('firstName') as string;
   console.info('firstName', firstName);
+  return { message: 'Profile Created' };
 };
 
 function CreateProfilePage() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold capitalize">new user</h1>
-      <div className="max-w-lg rounded-md border p-8">
-        <form action={createProfileAction}>
-          <div className="mb-2">
+      <div className="rounded-md border p-8">
+        <FormContainer action={createProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
             <FormInput type="text" name="firstName" label="First Name" />
             <FormInput type="text" name="lastName" label="Last Name" />
             <FormInput type="text" name="username" label="Username" />
           </div>
           <SubmitButton text="Create Profile" className="mt-8" />
-        </form>
+        </FormContainer>
       </div>
     </section>
   );
